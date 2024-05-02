0e2a17c8597f578501c7f3d4e4834d874a98cdc1
arzelcm <arzelcanavate@gmail.com>
2024-05-02 21:20:58 +0200

    Add some refactor

--- src/radix.c
+++ src/radix.c
@@ -6,7 +6,7 @@
 /*   By: arcanava <arcanava@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 18:03:30 by arcanava          #+#    #+#             */
-/*   Updated: 2024/05/02 20:08:57 by arcanava         ###   ########.fr       */
+/*   Updated: 2024/05/02 21:20:33 by arcanava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,25 @@
 #include "libft.h"
 #include "operations.h"
 
-int	get_min_stack_num(t_stack *stack)
-{
-	int	min;
-
-	min = stack->num;
-	while (stack)
-	{
-		if (min > stack->num)
-			min = stack->num;
-		stack = stack->next;
-	}
-	return (min);
-}
-
 void	sort_stack(t_stack **stack_a, t_stack **stack_b, int len)
 {
+	int	bit;
 	int	i;
-	int	j;
 
-	i = 1;
+	bit = 1;
 	while (!is_sorted_stack(*stack_a))
 	{
-		j = 0;
-		while (j < len && *stack_a)
+		i = 0;
+		while (i < len && *stack_a)
 		{
-			if ((*stack_a)->index & i)
+			if ((*stack_a)->index & bit)
 				rotate(stack_a, 'a');
 			else
 				push(stack_a, stack_b, 'b');
-			j++;
+			i++;
 		}
 		while (*stack_b)
 			push(stack_b, stack_a, 'a');
-		i = i << 1;
+		bit = bit << 1;
 	}
 }
